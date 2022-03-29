aa3746a8e2ed83301f195df3219469cdd4603bec
Bogdan Pyrc <pyrc.bogdan@gmail.com>
2022-03-29 10:34:51 +0200

    initial commit

---
+++ src/main/java/pl/finmatik/nbpcurrencyapi/NbpCurrencyApiApplication.java
@@ -0,0 +1,13 @@
+package pl.finmatik.nbpcurrencyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpCurrencyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbpCurrencyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/finmatik/nbpcurrencyapi/NbpCurrencyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.finmatik.nbpcurrencyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpCurrencyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
